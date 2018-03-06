40bc54a5065f7388cbeb4cc9de091d9092fb10ef
Joshua Howard <joshthoward@gmail.com>
2018-03-06 14:47:11 +0000

    More bindings autogenerated.

--- c_glib/example/rust/build.rs
+++ c_glib/example/rust/build.rs
@@ -1,14 +1,34 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-lib=bz2");
-
     let bindings = bindgen::Builder::default() 
         .clang_arg("-I")
         .clang_arg("../../")
+        .clang_arg("-I")
+        .clang_arg("/usr/include/glib-2.0/")
+        .clang_arg("-I")
+        .clang_arg("/usr/lib/x86_64-linux-gnu/glib-2.0/include/")
+        .blacklist_type("max_align_t")
         .generate_comments(false)
         .header("wrapper.h")
         .generate()
